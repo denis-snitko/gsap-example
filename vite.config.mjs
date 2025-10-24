2eac3115a45b32927a9544212c67c65b38993bd5
Denis Snitko <denissnitko@gmail.com>
2025-10-24 18:02:30 +0200

    Build

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "./", // <-- здесь важно, './' делает пути относительными
   build: {
-    outDir: "dist", // каталог для сборки
+    outDir: "docs", // каталог для сборки
   },
 });
